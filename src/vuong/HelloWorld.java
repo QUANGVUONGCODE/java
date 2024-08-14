560006880ca988ab0e98f46c9b733d277a19e5d4
QUANGVUONGCODE <vuongkaka0101@gmail.com>
2024-08-14 20:43:37 +0700

    init project

---
+++ src/vuong/HelloWorld.java
@@ -0,0 +1,12 @@
+package vuong;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+	}
+}
